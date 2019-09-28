bf2dbfd2f4fe8518990fc8a95361a5d26e3bc230
Shabbir <shabbirdwd53@gmail.com>
2019-09-28 16:37:08 +0530

    Property Expansion Code
    Property Expansion Code

---
+++ Spring-Boot-Tutorial/src/main/java/com/dailycodebuffer/examples/SpringBootTutorial/PropertyExpansion/PropertyExpansionSpringApplication.java
@@ -0,0 +1,45 @@
+package com.dailycodebuffer.examples.SpringBootTutorial.PropertyExpansion;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.Banner;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class PropertyExpansionSpringApplication {
+
+    @Bean
+    CustomBean customBean() {
+        return new CustomBean();
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        SpringApplication bootApp = new SpringApplication(PropertyExpansionSpringApplication.class);
+        bootApp.setBannerMode(Banner.Mode.OFF);
+        ConfigurableApplicationContext context = bootApp.run(args);
+        CustomBean customBean = context.getBean(CustomBean.class);
+        customBean.doSomething();
+    }
+
+    private static class CustomBean {
+
+        @Value("${project-name}")
+        private String projectName;
+
+        @Value("${spring-version}")
+        private String springVersion;
+
+        @Value("${app-title}")
+        private String appTitle;
+
+        public void doSomething() {
+            System.out.printf("Project name: %s%n"
+                            + "Spring version: %s%n"
+                            + "App title: %s%n",
+                    projectName, springVersion, appTitle);
+        }
+    }
+
+}
