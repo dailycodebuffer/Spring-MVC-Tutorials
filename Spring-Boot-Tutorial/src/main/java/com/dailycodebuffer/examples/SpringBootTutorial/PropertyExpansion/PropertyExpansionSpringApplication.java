6573ba01cece821f6c4595fa55d6eb6d4b15eeed
Shabbir <shabbirdwd53@gmail.com>
2019-09-28 16:37:27 +0530

    Update PropertyExpansionSpringApplication.java

--- Spring-Boot-Tutorial/src/main/java/com/dailycodebuffer/examples/SpringBootTutorial/PropertyExpansion/PropertyExpansionSpringApplication.java
+++ Spring-Boot-Tutorial/src/main/java/com/dailycodebuffer/examples/SpringBootTutorial/PropertyExpansion/PropertyExpansionSpringApplication.java
@@ -2,6 +2,7 @@ package com.dailycodebuffer.examples.SpringBootTutorial.PropertyExpansion;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.Banner;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
