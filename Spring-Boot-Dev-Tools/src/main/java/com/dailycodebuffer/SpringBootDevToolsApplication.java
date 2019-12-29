b2514f513a56b0a41c090b4067d53e8d4a57d040
Shabbir Dawoodi <shabbirdwd53@gmail.com>
2019-12-29 14:00:34 +0530

    Spring-Boot Dev Tools
    Spring-Boot Dev Tools

---
+++ Spring-Boot-Dev-Tools/src/main/java/com/dailycodebuffer/SpringBootDevToolsApplication.java
@@ -0,0 +1,13 @@
+package com.dailycodebuffer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDevToolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDevToolsApplication.class, args);
+	}
+
+}
